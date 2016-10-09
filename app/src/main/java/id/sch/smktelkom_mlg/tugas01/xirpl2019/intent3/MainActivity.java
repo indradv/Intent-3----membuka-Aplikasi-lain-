64c859cee7157b925e4fd759a14639d3dde77da2
USER-ACER-PC <nicothecruzguardian@gmail.com>
2016-10-09 15:31:32 +0700

    intent sms

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/intent3/MainActivity.java
@@ -12,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeSmsMessage("Pesan dari SMK Telkom Malang");
+                
+            }
+
+            
+        });
         findViewById(R.id.imageViewPhone)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -19,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
                         dialPhoneNumber("085649232533");
                     }
                 });
+    }
+
+    private void composeSmsMessage(String message) {
+        Intent intent =  new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain" );
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     private void dialPhoneNumber(String phoneNumber) {
